7384862c0e1979768685851410785dbf5fc543c0
Brvnaxs <xavierbruna9@gmail.com>
2025-05-28 14:42:32 -0300

    adiciona rotas de cadastro

--- estagios/__init__.py
+++ estagios/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['MAIL_SERVER']= 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'estagioparceiro@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wqxh xzvv xauw eyfw'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from estagios.rotas import homepage
+from estagios.rotas import confirmaEmail
+from estagios.rotas import cadastraUsuario
+from estagios.rotas import cadastraEstudante
--- estagios/rotas.py
+++ estagios/rotas.py
@@ -1,9 +1,72 @@
-from estagios import app
+from estagios import app, mail
+from flask import request
+import random
+from flask_mail import Message
+from estagios import db
+from estagios.models import User, RoleEnum, Estudante, Empresa
+
 
 @app.route('/')
 def homepage():
     return "Home Page"
 
-@app.route('/nova')
-def nova():
-    return "Nova paggina"
+@app.route('/cadastra_usuario', methods=['POST'])
+def cadastraUsuario():
+    dados = request.get_json()
+    novoUsuario = User(
+        email = dados.get('email'),
+        senha = dados.get('senha'),
+        role = RoleEnum(dados.get('role'))
+    )
+    db.session.add(novoUsuario)
+    db.session.commit()
+    return {'id': novoUsuario.id}
+
+@app.route('/confirma_email', methods=['POST'])
+def confirmaEmail():
+    dados = request.get_json()
+    id = dados.get('id')
+    codigo = ''.join(random.choice('0123456789') for _ in range(6))
+    usuario = User.query.get(id)
+    msg = Message(
+                subject = "Olá, confirme seu email.",
+                body = f"Seu código de confirmação: {codigo}",
+                sender = ('Estágio Parceiro | IFPE','estagioparceiro@gmail.com'),
+                recipients = [usuario.email]
+            )
+    mail.send(msg)
+    return f"Mensagem enviada para: {usuario.email}"
+
+
+@app.route('/cadastra_estudante', methods=['POST'])
+def cadastraEstudante():
+    dados = request.get_json()
+    usuario = User.query.get(dados.get('id'))
+    novoEstudante = Estudante(
+        user_id = usuario.id,
+        user_email = usuario.email,
+        nome = dados.get('nome'),
+        curriculo_profissional_link = dados.get('curriculo'),
+        telefone = dados.get('telefone'),
+        curso = dados.get('curso'),
+        periodo = dados.get('periodo')
+    )
+    db.session.add(novoEstudante)
+    db.session.commit()
+    return {'id': novoEstudante.id, 'user_id': novoEstudante.user_id}
+
+
+@app.route('\cadastra_empresa', methods=['POST'])
+def cadastraEmpresa():
+    dados = request.get_json()
+    usuario = User.query.get(dados.get('id'))
+    novaEmpresa = Empresa(
+        user_id = usuario.id,
+        CNPJ = dados.get('cnpj'),
+        endereco = dados.get('endereco'),
+        descricao = dados.get('descricao'),
+        telefone = dados.get('telefone')
+    )
+    db.session.add(novaEmpresa)
+    db.session.commit()
+    return {'id': novaEmpresa.id, 'user_id': usuario.id}
