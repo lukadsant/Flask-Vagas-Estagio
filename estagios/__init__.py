7509edab0befb782be7917713fa59eed8220e2f8
Bruna Xavier <bxs@discente.ifpe.edu.br>
2025-05-30 14:17:35 -0300

    adiciona arquivo .env

--- estagios/__init__.py
+++ estagios/__init__.py
@@ -2,16 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from dotenv import dotenv_values
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER']= 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'estagioparceiro@gmail.com'
-app.config['MAIL_PASSWORD'] = 'wqxh xzvv xauw eyfw'
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config.update(dotenv_values('estagios/.env'))
 mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
